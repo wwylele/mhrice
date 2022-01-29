663e99a33e9081ad860859cb768aea4dd61001a1
Weiyi Wang <wwylele@gmail.com>
2022-01-29 16:52:01 -0500

    add version for anger data

--- src/rsz/anger_data.rs
+++ src/rsz/anger_data.rs
@@ -3,7 +3,9 @@ use crate::rsz_struct;
 use serde::*;
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyAngerData.SeparateData")]
+    #[rsz("snow.enemy.EnemyAngerData.SeparateData",
+        0x9bb00587 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct EnemyAngerSeparateData {
         val: i32,
@@ -11,7 +13,9 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyAngerData")]
+    #[rsz("snow.enemy.EnemyAngerData",
+        0xfd3c0d3a = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct EnemyAngerData {
         data_info: Vec<Rc<EnemyAngerSeparateData>>,
