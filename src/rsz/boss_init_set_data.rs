4b978a664c50847a397dc1d294bc9af18895373a
Weiyi Wang <wwylele@gmail.com>
2022-01-30 11:05:58 -0500

    add version for boos init set data

--- src/rsz/boss_init_set_data.rs
+++ src/rsz/boss_init_set_data.rs
@@ -3,7 +3,9 @@ use crate::rsz_struct;
 use serde::*;
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyBossInitSetData.LotInfo")]
+    #[rsz("snow.enemy.EnemyBossInitSetData.LotInfo",
+        0x6b978bab = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct LotInfo {
         pub lot: i32,
@@ -13,7 +15,9 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyBossInitSetData.SetInfo")]
+    #[rsz("snow.enemy.EnemyBossInitSetData.SetInfo",
+        0x7fdbc9bb = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct SetInfo {
         pub set_name: String,
@@ -22,19 +26,23 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyBossInitSetData.StageInfo")]
+    #[rsz("snow.enemy.EnemyBossInitSetData.StageInfo",
+        0x1e7b6ae3 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct StageInfo {
-        pub map_type: i32,
+        pub map_type: i32, // snow.QuestMapManager.MapNoType
         pub set_info_list: Vec<SetInfo>,
     }
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyBossInitSetData")]
+    #[rsz("snow.enemy.EnemyBossInitSetData",
+        0x4cfd1fa0 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct EnemyBossInitSetData {
-        pub enemy_type: i32,
+        pub enemy_type: i32, // snow.enemy.EnemyDef.EnemyTypeIndex
         pub stage_info_list: Vec<StageInfo>,
     }
 }
