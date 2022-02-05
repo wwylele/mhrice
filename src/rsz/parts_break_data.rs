fb7db19a46b47daecba85fb3de3f1203366251e9
Weiyi Wang <wwylele@gmail.com>
2022-02-05 15:39:33 -0500

    add version to part break data

--- src/rsz/parts_break_data.rs
+++ src/rsz/parts_break_data.rs
@@ -3,7 +3,9 @@ use crate::rsz_struct;
 use serde::*;
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData.PartsLockParam")]
+    #[rsz("snow.enemy.EnemyPartsBreakData.PartsLockParam",
+        0xa98bb235 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct PartsLockParam {
         pub hash_value: u32,
@@ -11,7 +13,9 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData.PartsBreakData")]
+    #[rsz("snow.enemy.EnemyPartsBreakData.PartsBreakData",
+        0x4a6f1d70 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct PartsBreakData {
         pub parts_condition_id: i32,
@@ -22,7 +26,9 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData.ConditionPartsBreakData")]
+    #[rsz("snow.enemy.EnemyPartsBreakData.ConditionPartsBreakData",
+        0x1bba24cb = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct ConditionPartsBreakData {
         pub condition_id: i32,
@@ -31,17 +37,21 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData.PartsBreakGroupData")]
+    #[rsz("snow.enemy.EnemyPartsBreakData.PartsBreakGroupData",
+        0x28fa8520 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct PartsBreakGroupData {
-        pub parts_group: u16,
+        pub parts_group: u16, // snow.enemy.EnemyDef.PartsGroup
         pub parts_lock_group_hash: Vec<PartsLockParam>,
         pub condition_parts_break_data_list: Vec<ConditionPartsBreakData>,
     }
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData.PartsLossData")]
+    #[rsz("snow.enemy.EnemyPartsBreakData.PartsLossData",
+        0x2e4f23aa = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct PartsLossData {
         pub parts_condition_id: i32,
@@ -54,7 +64,9 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData.ConditionPartsLossData")]
+    #[rsz("snow.enemy.EnemyPartsBreakData.ConditionPartsLossData",
+        0x45e48140 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct ConditionPartsLossData {
         pub condition_id: i32,
@@ -63,17 +75,21 @@ rsz_struct! {
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData.PartsLossGroupData")]
+    #[rsz("snow.enemy.EnemyPartsBreakData.PartsLossGroupData",
+        0x845557b3 = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct PartsLossGroupData {
-        pub parts_group: u16,
+        pub parts_group: u16, // snow.enemy.EnemyDef.PartsGroup
         pub parts_lock_group_hash: Vec<PartsLockParam>,
         pub condition_parts_loss_data_list: Vec<ConditionPartsLossData>,
     }
 }
 
 rsz_struct! {
-    #[rsz("snow.enemy.EnemyPartsBreakData")]
+    #[rsz("snow.enemy.EnemyPartsBreakData",
+        0xd8364e1a = 0
+    )]
     #[derive(Debug, Serialize)]
     pub struct EnemyPartsBreakData {
         pub parts_break_group_data_list: Vec<PartsBreakGroupData>,
