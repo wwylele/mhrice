b65d4c40d2b94d9eb2480ced7e34e5071c935f03
Weiyi Wang <wwylele@gmail.com>
2022-02-18 19:33:17 -0500

    hash: simplify utf16 iteration

--- src/hash.rs
+++ src/hash.rs
@@ -1,11 +1,5 @@
 pub fn hash_as_utf16(s: &str) -> u32 {
-    fn iter(a: [u8; 2]) -> impl Iterator<Item = u8> {
-        std::iter::once(a[0]).chain(std::iter::once(a[1]))
-    }
-    let bytes: Vec<u8> = s
-        .encode_utf16()
-        .flat_map(|u| iter(u16::to_le_bytes(u)))
-        .collect();
+    let bytes: Vec<u8> = s.encode_utf16().flat_map(u16::to_le_bytes).collect();
     murmur3::murmur3_32(&mut &bytes[..], 0xFFFF_FFFF).unwrap()
 }
 
