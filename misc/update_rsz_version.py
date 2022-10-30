5367429cd3fc202acda717ddab023c79dfc85b8d
Weiyi Wang <wwylele@gmail.com>
2022-10-30 16:04:27 -0400

    add a script to automatically update rsz crc

---
+++ misc/update_rsz_version.py
@@ -0,0 +1,31 @@
+import sys
+import re
+import os
+
+mismatchregex = re.compile(".*Mismatch CRC (.*) for (.*)")
+
+mismatchlist = open(sys.argv[1])
+newversion = sys.argv[2]
+
+for line in mismatchlist.readlines():
+    m = mismatchregex.match(line)
+    if m is None:
+        continue
+    crc = m.group(1)
+    name = m.group(2)
+    print(f"{name} -> {crc}")
+
+    for source_file in os.listdir("src/rsz"):
+        source_file = "src/rsz/" + source_file
+        content = ""
+        source = open(source_file)
+        changed = False
+        for line in source.readlines():
+            content += line
+            if f"#[rsz(\"{name}" in line:
+                content += f"        0x{crc} = {newversion},\n"
+                changed = True
+        source.close()
+        source = open(source_file, "w")
+        source.write(content)
+        source.close()
